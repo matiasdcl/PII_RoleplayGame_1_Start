447fcb8aadfae73f95a01c51a5d31b0b32cce9d7
unknown <mateo.gutierrezo@correo.ucu.edu.uy>
2024-09-11 15:14:39 -0300

    clase Item implementada

--- src/Library/Item.cs
+++ src/Library/Item.cs
@@ -2,5 +2,25 @@ namespace Library;
 
 public class Item
 {
-    
+    public string Nombre { get; }
+    public int Defensa { get; }
+    public int Ataque { get; }
+
+    public Item(string nombre, int defensa, int ataque)
+    {
+        string caracteresPermitidos = "qQwWeErRtTyYuUiIoOpPsSaAdDFfGgHhJjKkLlÑñZzXxCcVvBbNnMm";
+        foreach (char caracter in nombre)
+        {
+            if(!caracteresPermitidos.Contains(caracter)){ 
+                throw new ArgumentException("Nombre inválido");
+            }
+        }
+        this.Nombre = nombre;
+        if (defensa < 0 || ataque < 0)
+        {
+            throw new AggregateException("Valor de defensa o ataque inválidos");
+        }
+        this.Defensa = defensa;
+        this.Ataque = ataque;
+    }
 }
