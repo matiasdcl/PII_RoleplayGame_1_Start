8e143f955cc5b884b87c88fb9ab3ce2e66b2b6e0
Ines Barral <barral.mariaines@gmail.com>
2024-09-15 19:10:26 -0300

    Se agrega código de prueba a Program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -5,29 +5,24 @@ public class Program
 {
     public static void Main()
     {
-         /*Item espada = new Item("Espada Cool", 0, 5000);
-         Console.WriteLine(espada.Ataque);
+         Item espada = new Item("Espada Cool", 0, 5000);
+         Console.WriteLine(espada.GetAtaque());
         //
-         Hechizo hechizo = new Hechizo("Revivir", 150, 0);
-         LibroDeHechizos libro = new LibroDeHechizos();
-         libro.AprenderHechizo(hechizo);
+
         
          Elfo legolas = new Elfo("Legolas");
          Console.WriteLine(legolas.GetAtaque());
          Console.WriteLine(legolas.GetVidaActual());
          legolas.EquiparItem(espada);
-         legolas.EquiparLibro(libro);
         
          Console.WriteLine(legolas.GetNombre());
          Console.WriteLine(legolas.GetAtaque());
-         Console.WriteLine(legolas.GetVidaActual());
-         legolas.UsarHechizo(hechizo);
         
          Enano krenko = new Enano("Krenko");
          krenko.AtacarElfo(legolas);
-         Console.WriteLine($"{legolas.GetVidaActual()}");
+         Console.WriteLine(legolas.GetVidaActual());
          legolas.AtacarEnano(krenko);
-         Console.WriteLine(krenko.Vida);*/
+         Console.WriteLine(krenko.GetVidaActual());
     }
 }
 
